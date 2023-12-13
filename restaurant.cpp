9b97d20fe639db2188604e5790cb44d276f4cd62
Nguyễn Thành Danh <nguyenthanhdanh23072004@gmail.com>
2023-12-14 00:09:32 +0700

    Complete encod binary name (haven't tested yet)

--- restaurant.cpp
+++ restaurant.cpp
@@ -97,13 +97,19 @@ public:
 class Huffman_AVL_Tree
 {
 private:
-	void rotateLeft(EncodingNode*&);
-	void rotateRight(EncodingNode*&);
-	void leftBalance(EncodingNode*&, int&);
-	void rightBalance(EncodingNode*&, int&);
+	vector<string> mapChar;
+	static void rotateLeft(EncodingNode*&);
+	static void rotateRight(EncodingNode*&);
+	static void leftBalance(EncodingNode*&, int&);
+	static void rightBalance(EncodingNode*&, int&);
 	EncodingNode* findUnbalancedNode(EncodingNode*);
+	static string findBinaryPath(EncodingNode*&, char);
+	static string binToDec(string);
+	static vector<string> createMapChar(EncodingNode*&);
+	static void encodeAllChar(EncodingNode*&, vector<char>&, vector<string>&);
 public:
-	void balanceTree(EncodingNode*, int&);
+	static void balanceTree(EncodingNode*, int&);
+	static string encode(EncodingNode*&, string);
 };
 
 class Customer
@@ -111,15 +117,14 @@ class Customer
 private:
 	int ID;
 	string originalName;
-	string encodedName;
+	string encodedNameBinary;
+	string encodedNameCaesar;
 	vector<pair<char, int>> listOfChar;
 	vector<pair<char, int>> generateOriginalName(string name);
-	vector<pair<char, int>> generateCaesarName(const vector<pair<char, int>>&);
 	void encodingCaesar(vector<pair<char, int>>&);
 	bool compareFreq(const std::pair<char, int> a, const std::pair<char, int> b);
-	bool compareEncodingNode(const EncodingNode* a, const EncodingNode* b);
 	void encodingHuffmanAVL();
-	void encodeToBinary();
+	string generateCaesarName(string name, vector<pair<char, int>>&);
 public:
 	int getID();
 	Customer(string);
@@ -196,12 +201,6 @@ bool Customer::compareFreq(const pair<char, int> a, const pair<char, int> b)
 	return false;
 }
 
-bool Customer::compareEncodingNode(const EncodingNode* a, const EncodingNode* b)
-{
-	if (a->freq < b->freq)
-		return true;
-}
-
 void Customer::encodingHuffmanAVL()
 {
 	// create a vector of EncodingNode* to store the list of character using for initialize the priority queue
@@ -214,7 +213,6 @@ void Customer::encodingHuffmanAVL()
 	}
 	int countNewNode = 0;
 	int countRotation;
-	Huffman_AVL_Tree checkTree;
 
 	while (pq.size() > 1)
 	{
@@ -224,18 +222,41 @@ void Customer::encodingHuffmanAVL()
 		EncodingNode* node2 = pq.top();
 		pq.pop();
 		EncodingNode* newNode = new EncodingNode('0', node1->freq + node2->freq, listOfChar.size() + countNewNode++, node1, node2, false);
-		checkTree.balanceTree(newNode, countRotation);
-		// Then balance the tree of newNode (This part will be implemented later)
+		Huffman_AVL_Tree::balanceTree(newNode, countRotation);
 		pq.push(newNode);
 	}
-	encodeToBinary();
+	EncodingNode* rootHuffmanAVL = pq.top(); // this is a tree
+	encodedNameBinary = Huffman_AVL_Tree::encode(rootHuffmanAVL, this->encodedNameCaesar);
 }
-
-void Customer::encodeToBinary()
+string Customer::generateCaesarName(string name, vector<pair<char, int>>&)
 {
-
+	string caesarName = "";
+	int shift = 0;
+	for (int i = 0; i < name.length(); i++)
+	{
+		for (pair<char, int> p : listOfChar)
+		{
+			if (p.first == name[i])
+			{
+				shift = p.second % 26;
+				break;
+			}
+		}
+		if (('a' <= name[i] && name[i] <= 'z') && (name[i] + shift > 'z'))
+		{
+			caesarName += 'a' + (name[i] + shift - 'z' - 1);
+		}
+		else if (('A' <= name[i] && name[i] <= 'Z') && (name[i] + shift > 'Z'))
+		{
+			caesarName += 'a' + (name[i] + shift - 'Z' - 1);
+		}
+		else
+		{
+			caesarName += name[i] + shift;
+		}
+	}
+	return caesarName;
 }
-
 vector<pair<char, int>> Customer::generateOriginalName(string name)
 {
 	// create a vector listchar contains frequency of each character in originalName
@@ -252,13 +273,6 @@ vector<pair<char, int>> Customer::generateOriginalName(string name)
 	return listchar;
 }
 
-vector<pair<char, int>> Customer::generateCaesarName(const vector<pair<char, int>>&)
-{
-	vector<pair<char, int>> newCustomerName;
-
-	return vector<pair<char, int>>();
-}
-
 int Customer::getID()
 {
 	return ID;
@@ -266,11 +280,11 @@ int Customer::getID()
 
 Customer::Customer(string name) : originalName(name)
 {
-	encodedName = "";
+	
 	// Create a vector listchar contains frequency of each character in originalName
 	listOfChar = generateOriginalName(originalName);
 	// Encoding the name using Caesar Cipher
-	//vector<pair<char, int>> encodeCaesar = encodingCaesar(listOfChar);
+	encodedNameCaesar = generateCaesarName(originalName, listOfChar);
 	// Create a vector listchar contains frequency of each character in encodedName
 	encodingCaesar(listOfChar);
 	// Sort the list of character by frequency
@@ -282,7 +296,7 @@ Customer::Customer(string name) : originalName(name)
 
 string Customer::getEncodedBinaryName()
 {
-	return encodedName;
+	return encodedNameBinary;
 }
 
 void Customer::encodingCaesar(vector<pair<char, int>>& listchar)
@@ -296,15 +310,15 @@ void Customer::encodingCaesar(vector<pair<char, int>>& listchar)
 		shift = listchar[i].second % 26;
 		if (('a' <= listchar[i].first && listchar[i].first <= 'z') && (listchar[i].first + listchar[i].second > 'z'))
 		{
-			newChar = 'a' + (listchar[i].first + listchar[i].second - 'z' - 1);
+			newChar = 'a' + (listchar[i].first + shift - 'z' - 1);
 		}
 		else if (('A' <= listchar[i].first && listchar[i].first <= 'Z') && (listchar[i].first + listchar[i].second > 'Z'))
 		{
-			newChar = 'a' + (listchar[i].first + listchar[i].second - 'Z' - 1);
+			newChar = 'a' + (listchar[i].first + shift - 'Z' - 1);
 		}
 		else
 		{
-			newChar = listchar[i].first + listchar[i].second;
+			newChar = listchar[i].first + shift;
 		}
 		// check if newChar is already in newCustomerName and add the frequency
 		for (pair<char, int> p : newCustomerName)
@@ -346,7 +360,7 @@ void Huffman_AVL_Tree::balanceTree(EncodingNode* root, int& countRotation)
 	int leftHeight = root->calculateHeight(root->left);
 	int rightHeight = root->calculateHeight(root->right);
 	int balanceFactor = leftHeight - rightHeight;
-	if (leftHeight == 0 && rightHeight == 0)
+	if (leftHeight == 0 && rightHeight == 0) // if the node is leaf
 	{
 		return;
 	}
@@ -365,6 +379,27 @@ void Huffman_AVL_Tree::balanceTree(EncodingNode* root, int& countRotation)
 	{
 		leftBalance(root, countRotation);
 	}
+}
+
+string Huffman_AVL_Tree::encode(EncodingNode*& root, string encodeName)
+{
+	string result = "";
+	vector<string> mapChar(52, "");
+	mapChar = createMapChar(root);
+	for (char c : encodeName)
+	{
+		if (c >= 'a' && c <= 'z')
+		{
+			result += mapChar[c - 'a'];
+		}
+		else if (c >= 'A' && c <= 'Z')
+		{
+			result += mapChar[c - 'A' + 26];
+		}
+	}
+	result = result.substr(0, 9);
+	result = binToDec(result);
+	return result;
 }
 
 void Huffman_AVL_Tree::rotateLeft(EncodingNode*& root)
@@ -418,6 +453,64 @@ void Huffman_AVL_Tree::rightBalance(EncodingNode*& root, int& countRotation)
 EncodingNode* Huffman_AVL_Tree::findUnbalancedNode(EncodingNode*)
 {
 	return nullptr;
+}
+
+string Huffman_AVL_Tree::findBinaryPath(EncodingNode*& root, char c)
+{
+	string binaryPath = "";
+	if (root->c != '0')
+	{
+
+	}
+	return "";
+}
+
+string Huffman_AVL_Tree::binToDec(string input)
+{
+	string output = "";
+	int dec = 0;
+	for (int i = 0; i < input.length(); i++)
+	{
+		dec += (input[i] - '0') * pow(2, input.length() - i - 1);
+	}
+	output = to_string(dec);
+	return output;
+}
+
+vector<string> Huffman_AVL_Tree::createMapChar(EncodingNode*& root)
+{
+	vector<string> myMap(52, "");
+	vector<char> path;
+	encodeAllChar(root, path, myMap);
+	return myMap;
+}
+
+void Huffman_AVL_Tree::encodeAllChar(EncodingNode*& root, vector<char>&path, vector<string> &map)
+{
+	if (root->left != nullptr && root->right != nullptr)
+	{
+		string decodePath(path.begin(), path.end());
+		if (root->c >= 'a' && root->c <= 'z')
+		{
+			map[root->c - 'a'] = decodePath;
+		}
+		else if (root->c >= 'A' && root->c <= 'Z')
+		{
+			map[root->c - 'A' + 26] = decodePath;
+		}
+	}
+	if (root->left != nullptr)
+	{
+		path.push_back('0');
+		encodeAllChar(root->left, path, map);
+		path.pop_back();
+	}
+	if (root->right != nullptr)
+	{
+		path.push_back('1');
+		encodeAllChar(root->right, path, map);
+		path.pop_back();
+	}
 }
 
 int EncodingNode::calculateBalanceFactor(EncodingNode* node)
